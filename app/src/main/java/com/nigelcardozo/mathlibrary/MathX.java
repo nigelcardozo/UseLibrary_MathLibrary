3199d06972f24c18e9c92adc1e57601c183c38ee
nigel cardozo <games@elnimijo.com>
2016-02-29 14:31:34 +0000

    Link Updates
    Updated Java file with links

--- app/src/main/java/com/nigelcardozo/mathlibrary/MathX.java
+++ app/src/main/java/com/nigelcardozo/mathlibrary/MathX.java
@@ -1,5 +1,16 @@
 package com.nigelcardozo.mathlibrary;
 
+//THIS GITHUB PROJECT RELATES TO THE FOLLOWING BLOG POSTS AT:
+//https://cakuma.wordpress.com/2016/02/29/android-studio-creating-a-library-project/
+//https://cakuma.wordpress.com/2016/02/29/android-studio-using-a-library-project/
+//Please visit cakuma for further details.
+
+//There is an accompany project at:
+//https://github.com/nigelcardozo/UseLibrary_MathApplication
+
+//If you find this helpful, please take a look at my apps :
+//https://play.google.com/store/apps/developer?id=elnimijo+games&hl=en_GB
+
 
 public class MathX {
 
