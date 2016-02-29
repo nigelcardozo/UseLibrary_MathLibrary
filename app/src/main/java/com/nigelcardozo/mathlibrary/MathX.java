b9d8747700c63b3fb217c6d89b4a9858c199b645
nigel cardozo <games@elnimijo.com>
2016-02-29 14:07:35 +0000

    Sample project showing how to create a library
    See accompanying blog post at
    
    https://cakuma.wordpress.com/2016/02/29/android-studio-creating-a-library-project/

---
+++ app/src/main/java/com/nigelcardozo/mathlibrary/MathX.java
@@ -0,0 +1,25 @@
+package com.nigelcardozo.mathlibrary;
+
+
+public class MathX {
+
+    public MathX(){
+
+    }
+
+    public int addX(int x, int y)
+    {
+        return (x+y);
+    }
+
+    public int subtractX(int x, int y)
+    {
+        return (x-y);
+    }
+
+    public int multiplyX(int x, int y)
+    {
+        return (x*y);
+    }
+
+}
